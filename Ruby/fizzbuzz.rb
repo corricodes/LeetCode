68d23062a1dfbf7585dad9f954fced039b3dffd4
Corrigan Baker <corriganbaker@gmail.com>
2022-10-12 10:43:18 -0400

    added fizzbuzz in ruby solution

---
+++ Ruby/fizzbuzz.rb
@@ -0,0 +1,48 @@
+# 412. Fizz Buzz
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["1","2","Fizz"]
+# Example 2:
+
+# Input: n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+# Example 3:
+
+# Input: n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+# Constraints:
+
+# 1 <= n <= 104
+
+# @param {Integer} n
+# @return {String[]}
+def fizz_buzz(n)
+    answer = []
+    counter = 1
+    
+    while counter <= n do
+        if counter % 15 == 0
+            answer.push("FizzBuzz")
+        elsif counter % 3 == 0
+            answer.push("Fizz")
+        elsif counter % 5 == 0
+            answer.push("Buzz")
+        else
+            answer.push(counter.to_s)
+        end
+        counter +=1
+    end
+    answer
+end
